2aad1ca5fc6cb1cb69213714f5455e59ca0594a6
Nicolas Perez <git@nicolasperez.com>
2017-04-04 11:17:00 +0200

    Part 2 : Models and the admin site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig', #"To include the app in our project, we need to add a reference to its configuration class in the INSTALLED_APPS setting."
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
